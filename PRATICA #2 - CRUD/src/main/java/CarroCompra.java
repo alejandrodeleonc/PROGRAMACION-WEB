7ab14b6bae53cbcd9f46369ba07199ecfc0429af
alejandrodeleonc <20160338@ce.pucmm.edu.do>
2020-06-16 13:33:50 -0400

    Comenzando practica 2

---
+++ PRATICA #2 - CRUD/src/main/java/CarroCompra.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+
+public class CarroCompra {
+    private long id;
+    private ArrayList<Producto> listaProductos;
+
+    public CarroCompra(long id, ArrayList<Producto> listaProductos) {
+        this.id = id;
+        this.listaProductos = listaProductos;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public ArrayList<Producto> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(ArrayList<Producto> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+}
---
+++ PRATICA #2 - CRUD/src/main/java/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        print("Hello world");
+    }
+    public static void print(String string){
+
+        System.out.println(string);
+    }
+
+}
